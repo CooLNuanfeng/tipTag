a827ae7f67608897970ea450f0a0bf80c98ce44e
吴虹松 <wuhongsong@wuhongsong-PC.(none)>
2014-08-23 13:19:58 +0800

    step_2

--- js/tipTag.js
+++ js/tipTag.js
@@ -9,10 +9,7 @@
 			height: 'auto',
 			bgColor : '#555',
 			textColor:'#fff',
-			padding: 20,  //设置内容部分的填充值
-			content : '相关内容相关内容相',
-			animate : 'slide',    //展开形式
-			className : 'arrowUp'  // 箭头方向
+			content : '相关内容相关内容相相关内容相关内容相相关内容相关内容相相关内容相关内容相相关内容相关内容相相关内容相关内容相相关内容相关内容相相关内容相关内容相'
 		}
 		
 		var settings = $.extend(defaults,options);
@@ -23,37 +20,36 @@
 			
 			$obj.mouseover(function(e){
 				
-				creatTip(settings.className, settings.content);
+				creatTip(settings.content);
 					
 				var L  = 0;
 				var T = 0
-				switch(settings.className){
-					case 'arrowUp':
-					
-						if( e.pageX > $(document).width()/2 ){
-							L = (e.pageX + settings.width > $(document).width() ?  $(document).width() - settings.width - settings.padding : e.pageX - settings.width/2)
-						}else{
-							L =  (e.pageX - settings.width/2 < 0 ? 0 : e.pageX - settings.width/2);
-						}
-						
-					 	T = e.pageY +  $(this).height() > $(document).height() ?  e.pageY - $('.tipTag').height() - $(this).height() : e.pageY +  $(this).height()
-						
-						
-						$('.tipTag').css({
-							width: settings.width,
-							height:settings.height,
-							left : L,
-							top : T
-						}).find('i').css({
-							left : e.pageX - L
-						})
-						break;
-						
-					case 'arrowDown':
-						
-						break;
+				
+				// 20 代表 padding 
+				if( e.pageX > $(document).width()/2 ){
+					L = (e.pageX + settings.width > $(document).width() ?  $(document).width() - settings.width - 20 : e.pageX - settings.width/2)
+				}else{
+					L =  (e.pageX - settings.width/2 < 0 ? 0 : e.pageX - settings.width/2);
 				}
 				
+				if( e.pageY +  $(this).height() > $(document).height() ){
+					T =  e.pageY - $('.tipTag').height() - $(this).height() - 18;  // 18 为微调值
+					$('.tipTag').find('i').removeClass('arrowUp').addClass('arrowDown');
+					
+				}else{
+					T = e.pageY +  $(this).height();
+				}
+				
+				
+				$('.tipTag').css({
+					width: settings.width,
+					height:settings.height,
+					left : L,
+					top : T
+				}).find('i').css({
+					left : e.pageX - L
+				})
+					
 			})
 			
 			$obj.mouseout(function(){
@@ -67,8 +63,8 @@
 		
 	}
 	
-	function creatTip(sClass,txt){
-		var strHtml = '<i class="'+sClass+'"></i><p>'+txt+'</p>'
+	function creatTip(txt){
+		var strHtml = '<i class="arrowUp"></i><p>'+txt+'</p>'
 		var $tip = $('<div class="tipTag"></div>').html(strHtml);
 		$('body').append($tip);
 	}
